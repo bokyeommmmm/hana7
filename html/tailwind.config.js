fb57ad832fe91c7e92af39437a65e27277f513a6
bokyeommmmm <12191732@inha.edu>
2025-04-01 10:18:58 +0900

    html_first_commit

---
+++ html/tailwind.config.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme:{
+        extend:{
+            colors: {
+                clifford: '#da373d',
+                carrot: '#e67e22',
+                emerald: '#2ecc71',
+
+            }
+        }
+    }
+}
