9e04f7a285ff2443161d442eceb6d3590dad5c50
bokyeommmmm <bokyeommmmm@github.com>
2025-04-15 11:32:13 +0900

    0415 ~array

--- js/0415/ex_array_2.js
+++ js/0415/ex_array_2.js
@@ -66,7 +66,6 @@ assert.deepStrictEqual(
   reduce(users, (acc, user) => acc + user.name),
   users.reduce((acc, user) => acc + user.name)
 );
-
 //-------------------------------------
 const arr = [1, 2, 3, 4, 5];
 const square = (n) => n ** 2;
@@ -88,3 +87,96 @@ const xr4 = arr.map((a) =>
   [square, cube, (n) => n + 1].reduce((acc, fn) => fn(acc), a)
 );
 console.log("🚀  xr4:", xr4);
+//----------------------------------------
+const range = (start, end, step = start > end ? -1 : 1) => {
+  if (step === 0 || start === end) return [start];
+  //   if (start > end && step > 0) return []; //(start-end) *step이 양수
+  //   if (start < end && step < 0) return []; //(start-end) *step이 양수 -> 한줄로 줄일수 있다.
+  if ((start - end) * step > 0) return []; //요놈처럼.
+  //
+  //
+  //아래 2중 if를 좀 어떻게 할수없을까 . 일단 통과시켜놓고 고민.
+  //   if (end === undefined) {
+  //     if (start > 0) {
+  //       end = start;
+  //       start = 1;
+  //     } else if (start < 0) {
+  //       end = -1;
+  //     } else {
+  //       return [0];
+  //     }
+  //   }
+  //아래처럼 고침
+  if (end === undefined && start === 0) return [0];
+  const t = start;
+  end = end ?? (start > 0 ? ((start = 1), t) : start < 0 ? -1 : 0);
+
+  const results = [];
+  for (let i = start; start > end ? i >= end : i <= end; i += step) {
+    results.push(i);
+  }
+
+  return results;
+};
+assert.deepStrictEqual(range(1, 10, 1), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+assert.deepStrictEqual(range(1, 10, 2), [1, 3, 5, 7, 9]);
+assert.deepStrictEqual(range(1, 10), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+assert.deepStrictEqual(range(10, 1), [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]);
+
+assert.deepStrictEqual(range(5, 5, 0), [5]);
+assert.deepStrictEqual(range(1, 5, 0), [1]);
+assert.deepStrictEqual(range(5, 5, -1), [5]);
+assert.deepStrictEqual(range(5, 5), [5]);
+assert.deepStrictEqual(range(0, 0, 5), [0]);
+assert.deepStrictEqual(range(1, 5, -1), []);
+
+assert.deepStrictEqual(range(1, 5, 6), [1]);
+assert.deepStrictEqual(range(0, 5), [0, 1, 2, 3, 4, 5]);
+assert.deepStrictEqual(range(-3, 0), [-3, -2, -1, 0]);
+
+assert.deepStrictEqual(range(5, 1, 1), []);
+assert.deepStrictEqual(range(0, -1), [0, -1]);
+assert.deepStrictEqual(range(0, -3), [0, -1, -2, -3]);
+assert.deepStrictEqual(range(5, 1), [5, 4, 3, 2, 1]);
+assert.deepStrictEqual(range(10, 1, -2), [10, 8, 6, 4, 2]);
+
+assert.deepStrictEqual(range(5), [1, 2, 3, 4, 5]);
+assert.deepStrictEqual(range(0), [0]);
+assert.deepStrictEqual(range(0, 0), [0]);
+assert.deepStrictEqual(range(2, 1, -5), [2]);
+assert.deepStrictEqual(range(0, -1, -5), [0]);
+assert.deepStrictEqual(range(-5), [-5, -4, -3, -2, -1]);
+assert.deepStrictEqual(
+  range(50),
+  Array.from({ length: 50 }, (_, i) => i + 1)
+);
+assert.deepStrictEqual(
+  range(1, 150, 3),
+  Array.from({ length: 50 }, (_, i) => i * 3 + 1)
+);
+//-------------------------------------
+
+function keyPairOnsquare(arr, sum) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === sum) return [i, j];
+    }
+  }
+}
+const keyPair = (arr, sum) => {
+  // 이건 이해해보자 .
+  const cache = {}; //value,
+  for (let i = 0; i < arr.length; i++) {
+    const value = arr[i];
+    if (cache[value]) return [cache[value], i];
+    cache[sum - value] = i;
+  }
+};
+assert.deepStrictEqual(keyPair([1, 3, 4, 5], 7), [1, 2]);
+assert.deepStrictEqual(keyPair([1, 4, 45, 6, 10, 8], 16), [3, 4]);
+assert.deepStrictEqual(keyPair([1, 2, 4, 3, 6], 10), [2, 4]);
+assert.deepStrictEqual(keyPair([1, 2, 3, 4, 5, 7], 9), [3, 4]);
+keyPair([1, 3, 4, 5], 7); // [1, 2]
+keyPair([1, 4, 45, 6, 10, 8], 16); // [3, 4]
+keyPair([1, 2, 4, 3, 6], 10); // [2, 4]
+keyPair([1, 2, 3, 4, 5, 7], 9); // [3, 4]  or [1, 5]
