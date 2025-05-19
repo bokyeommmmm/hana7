43e6d6a2f766be5d4c6ec03d38e0e709df9c3846
bokyeommmmm <bokyeommmmm@github.com>
2025-05-19 17:05:13 +0900

    0519

--- react/rbvite/src/components/Profile.tsx
+++ react/rbvite/src/components/Profile.tsx
@@ -1,5 +1,7 @@
-import type { RefObject } from "react";
+import { useEffect, type RefObject } from "react";
 import { useSession } from "../contexts/session/SessionContext";
+import Trans from "./Trans";
+import Posts from "./Posts";
 
 type Props = {
   logoutButtonRef: RefObject<HTMLButtonElement | null>;
@@ -11,8 +13,15 @@ export default function Profile({ logoutButtonRef }: Props) {
     logout,
   } = useSession();
 
+  console.log("ffff");
+  const f = () => console.log("sss > ", loginUser?.name);
+  useEffect(() => {
+    f();
+  }, [f]);
   return (
     <>
+      <Trans />
+      <Posts />
       <h3>LoginUser: {loginUser?.name}</h3>
       <button ref={logoutButtonRef} onClick={logout}>
         LogOut
